9a61f0d2425abde0bae2401c7c008d51ad8b18e1
Ernest Lasten <lastenernest@gmail.com>
2023-06-07 20:41:46 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import navigation from './modules/navigation.js';
-import addBook from './modules/addBook.js';
-import booksStoredData from './modules/booksStoredData.js';
-import createDate from './modules/date.js';
-
-navigation();
-booksStoredData();
-addBook();
-createDate();
